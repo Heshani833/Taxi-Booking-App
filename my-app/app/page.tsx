7c1b2714835d696388bc92467fa0619c426b23fe
Heshani833 <heshanishehana123@gmail.com>
2026-02-06 06:05:21 -0800

    Custom Font

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
    <div>
+    <UserButton afterSignOutUrl="/" />
     <h1>Welcome to My App</h1>
    </div>
   );
